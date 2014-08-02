59e7c6bca762e9f6f7c89a45c8590c673a0a175d
Firef0x <Firefgx@gmail.com>
2014-08-02 15:37:42 +0800

    Update build.bat 更新 build.bat 1. 增加 DIRECTX=yes 编译参数以开启 directx 特性 2. 修正第二个 DYNAMIC_PYTHON 为 DYNAMIC_PYTHON3

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,5 @@
 :: command to build big Vim with Python and Lua
 @echo off
 setlocal
-nmake -f Make_mvc.mak GUI=yes MBYTE=yes IME=yes GIME=yes CSCOPE=yes DEBUG=no PYTHON=C:\Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=C:\Python34 DYNAMIC_PYTHON=yes PYTHON3_VER=34 LUA="D:\codes\C++\lib\lua\src" DYNAMIC_LUA=yes LUA_VER=52 CPUNR=i686 WINVER=0x0500 XPM="D:\codes\C++\fork\vim\src\xpm\x86" USERNAME=F USERDOMAIN=F %*
+nmake -f Make_mvc.mak GUI=yes MBYTE=yes IME=yes GIME=yes CSCOPE=yes DIRECTX=yes DEBUG=no PYTHON=C:\Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=C:\Python34 DYNAMIC_PYTHON3=yes PYTHON3_VER=34 LUA="D:\codes\C++\lib\lua\src" DYNAMIC_LUA=yes LUA_VER=52 CPUNR=i686 WINVER=0x0500 XPM="D:\codes\C++\fork\vim\src\xpm\x86" USERNAME=F USERDOMAIN=F %*
 endlocal
